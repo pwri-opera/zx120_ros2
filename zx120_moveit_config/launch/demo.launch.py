6a5c736b7ae48f13b66ef7c0ac00f173f041e25c
ChiroruNeko <koshi2660@gmail.com>
2022-11-18 15:42:59 +0900

    Fix to make the launch file simple

--- zx120_moveit_config/launch/demo.launch.py
+++ zx120_moveit_config/launch/demo.launch.py
@@ -15,81 +15,8 @@ def generate_launch_description():
 
     robot_name = "zx120"
     moveit_config = (
-        # Simplify loading config
         MoveItConfigsBuilder(
-            robot_name=robot_name, package_name=robot_name+"_moveit_config").to_moveit_configs()
+            robot_name=robot_name, package_name=robot_name+"_moveit_config")
+        .to_moveit_configs()
     )
-    # return generate_demo_launch(moveit_config)
-
-    # Start the actual move_group node/action server
-    move_group_node = Node(
-        package="moveit_ros_move_group",
-        executable="move_group",
-        output="screen",
-        parameters=[moveit_config.to_dict()],
-    )
-
-    # RViz
-    rviz_config = os.path.join(
-        get_package_share_directory(robot_name+"_moveit_config"),
-        "launch/moveit.rviz",
-    )
-    rviz_node = Node(
-        package="rviz2",
-        executable="rviz2",
-        name="rviz2",
-        output="log",
-        arguments=["-d", rviz_config],
-        parameters=[
-            moveit_config.robot_description,
-            moveit_config.robot_description_semantic,
-            moveit_config.planning_pipelines,
-            moveit_config.robot_description_kinematics,
-        ],
-    )
-    # Publish TF
-    robot_state_publisher = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        name="robot_state_publisher",
-        output="both",
-        parameters=[moveit_config.robot_description],
-    )
-
-    # ros2_control using FakeSystem as hardware
-    ros2_controllers_path = os.path.join(
-        get_package_share_directory(robot_name+"_moveit_config"),
-        "config/",
-        "ros2_controllers.yaml",
-    )
-    ros2_control_node = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
-        parameters=[moveit_config.robot_description, ros2_controllers_path],
-        output="both",
-    )
-
-    # Load controllers
-    load_controllers = []
-    for controller in [
-        "joint_state_broadcaster",
-        "upper_arm_controller",
-    ]:
-        load_controllers += [
-            ExecuteProcess(
-                cmd=["ros2 run controller_manager spawner {}".format(
-                    controller)],
-                shell=True,
-                output="screen",
-            )
-        ]
-
-    return LaunchDescription(
-        [
-            rviz_node,
-            robot_state_publisher,
-            move_group_node,
-            ros2_control_node,
-        ]
-        + load_controllers
-    )
+    return generate_demo_launch(moveit_config)
