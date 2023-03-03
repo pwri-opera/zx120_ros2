3d7d5242e0351d2accf2be62ddc77c2d394dff95
maopotcha <endou-d177cl@pwri.go.jp>
2023-03-03 10:35:35 +0900

    Fix bug

--- zx120_bringup/scripts/poseStamped2Odometry.py
+++ zx120_bringup/scripts/poseStamped2Odometry.py
@@ -55,70 +55,72 @@ class Pose2odom(Node):
         # self.i += 1
         # rate = rospy.Rate(50.0)
 
-        while rclpy.ok():
-            (v_roll, v_pitch, v_yaw) = self.quaternion_to_euler_angle(pose.pose.orientation.w, pose.pose.orientation.x, pose.pose.orientation.y, pose.pose.orientation.z)
-            #(v_roll, v_pitch, v_yaw) = self.quaternion_to_euler_angle()
-            v_phi = float((v_roll))
-            v_theta = float((v_pitch))
-            v_psi = float((v_yaw))
+        # while rclpy.ok():
+        (v_roll, v_pitch, v_yaw) = self.quaternion_to_euler_angle(pose.pose.orientation.w, pose.pose.orientation.x, pose.pose.orientation.y, pose.pose.orientation.z)
+        #(v_roll, v_pitch, v_yaw) = self.quaternion_to_euler_angle()
+        v_phi = float((v_roll))
+        v_theta = float((v_pitch))
+        v_psi = float((v_yaw))
 
-            x = pose.pose.position.x
-            y = pose.pose.position.y
-            z = pose.pose.position.z
+        x = pose.pose.position.x
+        y = pose.pose.position.y
+        z = pose.pose.position.z
 
-            yaw = math.radians(v_psi)
+        yaw = math.radians(v_psi)
 
-            if self.i > 0:
-                vel_x_world = (x - x_prev) / self.dt
-                vel_y_world = (y - y_prev) / self.dt
+        if self.i > 0:
+            self.get_logger().info("%s in timer_cb: z=" % (pose.pose.position.z))
+            vel_x_world = (x - self.x_prev) / self.dt
+            vel_y_world = (y - self.y_prev) / self.dt
 
-                x_prev = x
-                y_prev = y
+            self.x_prev = x
+            self.y_prev = y
 
-                twist_x = math.cos(yaw) * vel_x_world + math.sin(yaw) * vel_y_world
-                twist_y = math.cos(yaw) * vel_y_world - math.sin(yaw) * vel_x_world
+            twist_x = math.cos(yaw) * vel_x_world + math.sin(yaw) * vel_y_world
+            twist_y = math.cos(yaw) * vel_y_world - math.sin(yaw) * vel_x_world
 
-                odom = Odometry()
-                odom.header.frame_id = 'world'
-                odom.child_frame_id = 'gnss/base_link'
-                odom.header.stamp = self.get_clock().now().to_msg()
+            odom = Odometry()
+            odom.header.frame_id = 'world'
+            odom.child_frame_id = 'gnss/base_link'
+            odom.header.stamp = self.get_clock().now().to_msg()
 
-                odom.pose.pose.position.x = pose.pose.position.x
-                odom.pose.pose.position.y = pose.pose.position.y
-                odom.pose.pose.position.z = pose.pose.position.z
+            odom.pose.pose.position.x = pose.pose.position.x
+            odom.pose.pose.position.y = pose.pose.position.y
+            odom.pose.pose.position.z = pose.pose.position.z
 
-                odom.pose.pose.orientation.x = pose.pose.orientation.x
-                odom.pose.pose.orientation.y = pose.pose.orientation.y
-                odom.pose.pose.orientation.z = pose.pose.orientation.z
-                odom.pose.pose.orientation.w = pose.pose.orientation.w
+            odom.pose.pose.orientation.x = pose.pose.orientation.x
+            odom.pose.pose.orientation.y = pose.pose.orientation.y
+            odom.pose.pose.orientation.z = pose.pose.orientation.z
+            odom.pose.pose.orientation.w = pose.pose.orientation.w
 
-                odom.twist.twist.linear.x = twist_x
-                odom.twist.twist.linear.y = twist_y
-                odom.twist.twist.linear.z = (z - z_prev) / self.dt
-                z_prev = z
+            odom.twist.twist.linear.x = twist_x
+            odom.twist.twist.linear.y = twist_y
+            odom.twist.twist.linear.z = (z - self.z_prev) / self.dt
+            self.z_prev = z
 
-                odom.twist.twist.angular.x = 0.
-                odom.twist.twist.angular.y = 0.
-                odom.twist.twist.angular.z = 0.
+            odom.twist.twist.angular.x = 0.
+            odom.twist.twist.angular.y = 0.
+            odom.twist.twist.angular.z = 0.
 
-                global is_sub
+            global is_sub
 
-                if is_sub == True:
-                    self.publisher_.publish(odom)
-                    # br = tf.TransformBroadcaster()
-                    # br.sendTransform((x, y, z), [pose.pose.orientation.x, pose.pose.orientation.y,
-                    #                          pose.pose.orientation.z, pose.pose.orientation.w], rospy.Time.now(), "gnss/base_link", "map")
-                    is_sub = False
+            if is_sub == True:
+                self.publisher_.publish(odom)
+                # br = tf.TransformBroadcaster()
+                # br.sendTransform((x, y, z), [pose.pose.orientation.x, pose.pose.orientation.y,
+                #                          pose.pose.orientation.z, pose.pose.orientation.w], rospy.Time.now(), "gnss/base_link", "map")
+                is_sub = False
 
-            else:
-                x_prev = x
-                y_prev = y
-                z_prev = z
-                self.i += 1
+        else:
+            self.x_prev = x
+            self.y_prev = y
+            self.z_prev = z
+            self.i += 1
 
     def pose_callback(self, msg):
         global pose
         pose = msg
+        self.get_logger().info("%s in pose_cb: z=" % (pose.pose.position.z))
         global is_sub
         is_sub = True
 
